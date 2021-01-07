2ef72201fb1b25b1e8ff1fb8afcb6518b9205b19
Wisman Nur <wismannura@gmail.com>
2021-01-07 10:34:03 +0700

    add vue-fragment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,41 @@
 require('events').EventEmitter.defaultMaxListeners = 10
+const webpack = require('webpack')
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'midone-admin-cpnsonline',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        charset: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: '',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico',
+      },
+      // {
+      //   href: '/css/app.css',
+      // },
+    ],
+    script: [],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: ['~/plugins/vue-fragment'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -27,7 +47,7 @@ export default {
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
     // https://tailwindcss.com/docs/guides/nuxtjs
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
@@ -42,5 +62,12 @@ export default {
   axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    vendor: ['jquery'],
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+      }),
+    ],
+  },
 }
