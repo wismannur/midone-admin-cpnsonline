7d631f4cdcb50201b3ea6af0324aa442e6cc98b7
Wisman Nur <wismannura@gmail.com>
2021-01-07 10:49:50 +0700

    add new page for 404 error page not found

---
+++ pages/_.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <!-- BEGIN: Error Page -->
+    <div
+      class="error-page flex flex-col lg:flex-row items-center justify-center h-screen text-center lg:text-left"
+    >
+      <div class="-intro-x lg:mr-20">
+        <img
+          alt="Midone Tailwind HTML Admin Template"
+          class="h-48 lg:h-auto"
+          src="/images/error-illustration.svg"
+        />
+      </div>
+      <div class="text-white mt-10 lg:mt-0">
+        <div class="intro-x text-8xl font-medium">404</div>
+        <div class="intro-x text-xl lg:text-3xl font-medium mt-5">
+          Oops. This page has gone missing.
+        </div>
+        <div class="intro-x text-lg mt-3">
+          You may have mistyped the address or the page may have moved.
+        </div>
+        <button
+          class="intro-x button button--lg border border-white dark:border-dark-5 dark:text-gray-300 mt-10"
+          @click="$router.push('/')"
+        >
+          Back to Home
+        </button>
+      </div>
+    </div>
+    <!-- END: Error Page -->
+  </div>
+</template>
